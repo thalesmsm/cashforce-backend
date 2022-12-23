3329da365b570eae17c0363f667dc0fa18e3f42c
Thales Maciel <thalesmsm12@gmail.com>
2022-12-23 18:25:28 -0300

    initial config sequelize, express n' db

---
+++ src/api/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const buyersController = require('../controllers/buyers.controller');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/buyers', buyersController.getBuyers);
+
+module.exports = app;
